c18945c76e6dab8ea321e20ab7990abe24054de9
Qoosim <plenty4luv@yahoo.com>
2019-12-31 08:31:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move?(board, index)
   if position_taken?(board, index)
-    board[index].nil || board[index] == ""
+    !(board[index].nil || board[index] == "")
   else
     false
   end
